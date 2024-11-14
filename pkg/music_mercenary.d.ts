0344fa2eaec8ff62d366d9c2680da7693facfd02
Gabriel Nodarse <gabenodarse@yahoo.com>
2024-11-14 00:21:55 -0500

    update

--- pkg/music_mercenary.d.ts
+++ pkg/music_mercenary.d.ts
@@ -44,6 +44,21 @@ export function graphic_size(g: GraphicGroup): Position;
 export function num_graphic_groups(): number;
 /**
 */
+export enum GraphicFlags {
+  HorizontalFlip = 1,
+  VerticalFlip = 2,
+  Opacity = 4,
+}
+/**
+*/
+export enum Input {
+  Dash = 0,
+  Slash1 = 1,
+  Slash2 = 2,
+  Slash3 = 3,
+}
+/**
+*/
 export enum BrickType {
   Type1 = 0,
   Type2 = 1,
@@ -78,21 +93,6 @@ export enum GraphicGroup {
   Hold1 = 23,
   Hold2 = 24,
   Hold3 = 25,
-}
-/**
-*/
-export enum GraphicFlags {
-  HorizontalFlip = 1,
-  VerticalFlip = 2,
-  Opacity = 4,
-}
-/**
-*/
-export enum Input {
-  Dash = 0,
-  Slash1 = 1,
-  Slash2 = 2,
-  Slash3 = 3,
 }
 /**
 */
